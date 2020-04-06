98f414ad887455f144e2baa83664df5629bc8392
Ville Häyrynen <hayrynenville@outlook.com>
2020-04-06 08:59:00 +0300

    modified views and added style.css

--- application/views/pages/home.php
+++ application/views/pages/home.php
@@ -1,1 +1,13 @@
-This is home
+<h2><?= $title ?></h2>
+<p>Welcome to OUBusSH</p>
+
+        <div class="schedule-content">
+            <h2>Next arrivals:</h2>
+            <table>
+                <tr>
+                    <th>Bus</th>
+                    <th>Time of arrival</th>
+                    <th>Time until arrival</th>
+                </tr>
+            </table>
+        </div>
--- application/views/templates/header.php
+++ application/views/templates/header.php
@@ -1,7 +1,23 @@
 <!DOCTYPE html>
 <html>
 	<head>
-		<meta charset="UTF-8">
 		<title>Oulu University Bus Stop Helper</title>
+		<link rel="stylesheet" href="https://bootswatch.com/4/cerulean/bootstrap.min.css">
+		<link rel="stylesheet" href="<?php echo base_url(); ?>assets/css/style.css">
 	</head>
 <body>
+<nav class="navbar navbar-inverse">
+	<div class="container">
+		<div class="navbar-header">
+			<a class="navbar-brand" href="<?php echo base_url(); ?>">OUBuSH</a>
+		</div>
+		<div id="navbar">
+			<ul class="nav navbar-nav">
+				<li><a href="<?php echo base_url(); ?>">Home</a></li>
+				<li><a href="<?php echo base_url(); ?>/about">About</a></li>
+			</ul>
+		</div>
+	</div>
+</nav>
+
+<div class="container">
