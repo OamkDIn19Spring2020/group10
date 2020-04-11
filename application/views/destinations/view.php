c4da3849347f65c5eb14d90c492c3c9b078ef8d8
Mikael Pursiainen <pursiainen.mikael@gmail.com>
2020-04-11 15:21:32 +0300

    Modified the site to work in general

---
+++ application/views/destinations/view.php
@@ -0,0 +1,8 @@
+<h2><?php echo $destination['destination_name']; ?></h2>
+<div class="container">
+    <?php echo $destination['busses']; ?>
+</div>
+<br>
+<div class="big-button">
+    <a href="<?php echo base_url(); ?>destinations"><h2>Go Back</h2></a>
+</div>
