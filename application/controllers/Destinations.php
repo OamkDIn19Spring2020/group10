f780fcb9aabc5f16e8da9e3b25596a84e3b87477
Ville Häyrynen <hayrynenville@outlook.com>
2020-04-11 14:53:14 +0300

    Working destinations menu and views, added css styling

--- application/controllers/Destinations.php
+++ application/controllers/Destinations.php
@@ -10,4 +10,18 @@
 			$this->load->view('destinations/index', $data);
 			$this->load->view('templates/footer');
 		}
+
+		public function view($slug = NULL){
+			$data['destination'] = $this->destination_model->get_destinations($slug);
+
+			if(empty($data['destination'])){
+				show_404();
+			}
+
+			$data['title'] = $data['destination']['destination_name'];
+
+			$this->load->view('templates/header');
+			$this->load->view('destinations/view', $data);
+			$this->load->view('templates/footer');
+		}
 	}
--- application/views/destinations/index.php
+++ application/views/destinations/index.php
@@ -3,7 +3,9 @@
 	<div class="container">
 		<div class="destination-selector">
 			<?php foreach($destinations as $destination) : ?>
+				<a href="<?php echo site_url('/destinations/'.$destination['slug']); ?>">
 				<h4 class="destination-item"><?php echo $destination['destination_name']; ?></h4>
+				</a>
 			<?php endforeach; ?>
 		</div>
 	</div>
