c4bf252b083b567fdba7f4e36db977fee17ac75c
Jani Inkala <t9inja00@students.oamk.fi>
2020-04-27 21:38:35 +0300

    kaikki levällään

--- application/views/templates/header.php
+++ application/views/templates/header.php
@@ -1,3 +1,6 @@
+<?php
+session_start()
+?>
 <!DOCTYPE html>
 <html>
 	<head>
@@ -6,6 +9,11 @@
 		<link rel="stylesheet" href="<?php echo base_url(); ?>/assets/css/style.css">
 	</head>
 <body>
+
+<?php
+"echo <body style='background-color:".$_SESSION['page_color']."'>";
+?>
+
 <nav class="navbar navbar-inverse">
 	<div class="container">
 		<div class="navbar-header">
@@ -15,9 +23,31 @@
 			<ul class="nav navbar-nav">
 				<li><a href="<?php echo base_url(); ?>">Home</a></li>
 				<li><a href="<?php echo base_url(); ?>about">About</a></li>
+				<button type="button" id="theme_change" class="theme">Theme change</button>
 			</ul>
 		</div>
 	</div>
 </nav>
+
+<script>
+
+document.getElementById("theme_change").onclick = function() {theme_changer()} ;
+
+function theme_changer()
+{
+	if(document.body.style.backgroundColor === "black")
+	{
+		document.body.style.backgroundColor = "white";
+		<?php $_SESSION['page_color'] = "white";?>;
+	}
+	else
+	{
+		document.body.style.backgroundColor = "black";
+		<?php $_SESSION['page_color'] = "black";?>
+	}
+	
+}
+
+</script>
 
 <div class="container">
